0ec4b400f48886063fd08bd2d6399e4713812987
Sergio Santoyo <ssantoyo@fb.com>
2020-06-23 13:49:29 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by Sergio Santoyo on 6/23/20.
+//  Copyright © 2020 ssantoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tipster/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Tipster
+//
+//  Created by Sergio Santoyo on 6/23/20.
+//  Copyright © 2020 ssantoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
